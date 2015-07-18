6e9fc727658df3460688dedca726773b53543b7e
Shuichi Komiya <shu@komish.jp>
2015-07-18 18:28:47 +0900

    modify tregex demo

--- Demo/tregex demo/Unit1.pas
+++ Demo/tregex demo/Unit1.pas
@@ -3,7 +3,7 @@ unit Unit1;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, ComCtrls;
 
 type
